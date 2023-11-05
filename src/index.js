2896a090c0ec3fa9d120106f3b213655dc5d936a
Christopher Brum <christopher.brum@gmail.com>
2023-11-04 17:31:55 -0700

    added the client library code to test, it worked until it didn't

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-const express = require('express');
+// const express = require('express');
+import express from 'express';
 const app = express();
 
-const cors = require('cors');
+import cors from "cors";
 app.use(cors());
 
 app.use(express.static('./public'));
 
-const PORT = 3002;
+const PORT = 4000;
 
 app.get('/', (req, res) => {
   console.log("this route is being run");
